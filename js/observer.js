e43cb30a2fdbdcad6532192970a7035dca5bdc27
Miguel Angel Ramos <return.infox@gmail.com>
2023-12-26 19:01:33 -0300

    patron observer

---
+++ js/observer.js
@@ -0,0 +1,28 @@
+class Subject {
+
+  observers = [];
+  constructor() {
+    this.observers = [];
+  }
+
+  //* Método para subscribirse
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+
+  //* Eliminar la Subscripción
+  unsubscribe(observer) {
+    this.observers = this.observers.filter( obs => obs !== observer);
+    // this.observers = this.observers.filter( obs => {
+    //   return obs !== observer;
+    // });
+  }
+
+  //* Método que notifica
+  notify(data) {
+    this.observers.forEach( e => {
+      e.refresh(data)
+    });
+  }
+
+}
